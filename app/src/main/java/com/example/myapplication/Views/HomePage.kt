8b2a32c6ae14620ae5114bda3ec3fb3898ca627d
Edi Ginting <ediginting3421@gmail.com>
2024-11-20 00:19:03 +0700

    Buat Bottom NavBar

--- app/src/main/java/com/example/myapplication/Views/HomePage.kt
+++ app/src/main/java/com/example/myapplication/Views/HomePage.kt
@@ -1,20 +1,25 @@
 package com.example.myapplication.Views;
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.tooling.preview.Preview // Import Preview annotation
 
 @Composable
-fun Home() {
+fun HomePage() {
     Surface(color = MaterialTheme.colorScheme.background) {
         Column(
             modifier = Modifier
@@ -23,7 +28,7 @@ fun Home() {
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-            val textState = remember { mutableStateOf("Hello, Jetpack Compose!") }
+            val textState = remember { mutableStateOf("Barnibar") }
 
             Text(text = textState.value)
             Spacer(modifier = Modifier.height(100.dp))
@@ -38,5 +43,5 @@ fun Home() {
 @Preview(showBackground = true)
 @Composable
 fun PreviewHome() {
-    Home()
+    HomePage()
 }
--- app/src/main/java/com/example/myapplication/Views/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/Views/MainActivity.kt
@@ -1,39 +1,18 @@
 package com.example.myapplication.Views
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.myapplication.R
-import com.google.firebase.auth.FirebaseAuth
+import com.example.myapplication.Views.NavBar.NavBar
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
 
-        auth = FirebaseAuth.getInstance()
 
-        val logoutButton = findViewById<Button>(R.id.logoutButton)
-        logoutButton.setOnClickListener {
-            auth.signOut()
-
-            // ke logout
-            val intent = Intent(this, SignInActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContent {
+            NavBar()
         }
     }
 }
